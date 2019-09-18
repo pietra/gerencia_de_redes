8afae9714e7e9fe9048ad440c825090576cf44d0
Lucas Hagen <lucas_hagen@hotmail.com>
2019-09-17 23:18:46 -0300

    Fixed time axis
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

--- main.py
+++ main.py
@@ -22,9 +22,12 @@ def main():
     in_out_rates(agente)
     in_out_error_rates(agente)
 
+    times = []
+    start_time = time.time()
     while(True):
         rate_in, rate_out = in_out_rates(agente)
         err_rate_in, err_rate_out = in_out_error_rates(agente)
+        times.append(time.time() - start_time)
 
         agente.rate_in.append(rate_in)
         agente.rate_out.append(rate_out)
@@ -32,7 +35,7 @@ def main():
         agente.error_rate_in.append(err_rate_in)
         agente.error_rate_out.append(err_rate_out)
 
-        plot_rate(agente.rate_in, agente.rate_out,
+        plot_rate(times, agente.rate_in, agente.rate_out,
                   agente.error_rate_in, agente.error_rate_out)
 
         time.sleep(5)
--- plot.py
+++ plot.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 import time
 
-def plot_rate(rate_in, rate_out, err_rate_in, err_rate_out):
+def plot_rate(times, rate_in, rate_out, err_rate_in, err_rate_out):
 
     plt.subplot(2, 2, 1)
-    plt.plot(rate_in)
+    plt.plot(times, rate_in)
     plt.title('In and Out Traffic')
     plt.ylabel('Download (Mbps)')
 
     plt.subplot(2, 2, 3)
-    plt.plot(rate_out)
+    plt.plot(times, rate_out)
     plt.xlabel('Time (s)')
     plt.ylabel('Upload (Mbps)')
 
     plt.subplot(2, 2, 2)
-    plt.plot(err_rate_in)
+    plt.plot(times, err_rate_in)
     plt.title('Errors')
     plt.ylabel('In Packets with Error')
 
     plt.subplot(2, 2, 4)
-    plt.plot(err_rate_out)
+    plt.plot(times, err_rate_out)
     plt.xlabel('time (s)')
     plt.ylabel('Out Packets with Error')
 
